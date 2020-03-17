edda8e22d6ae3f86a5633b88edd1b789bf770491
Ricardo Jesus <rdo_ramone@hotmail.com>
2020-03-16 22:35:06 -0300

    Adicionando módulo para carregar todos os módulos ao fazer o primeiro carregamento do app

--- meat-app-starter-master/src/app/app.module.ts
+++ meat-app-starter-master/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
@@ -43,7 +43,10 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpModule,
     SharedModule.forRoot(),
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
+    /*
+     Aqui o PreloadAllModules faz o carregamento dos módulos lazy loading logo após do primeiro carregamento.
+    */
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' }
