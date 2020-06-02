fcce197575fb463b7bcaeb6c163f90dca5934c7f
Ricardo Jesus <rdo_ramone@hotmail.com>
2020-06-02 15:47:36 -0300

    Adicionando segurança e automatização para o processo de backend.

---
+++ meat-app-starter-master/backend/dist/server.js
@@ -0,0 +1,27 @@
+"use strict";
+exports.__esModule = true;
+var jsonServer = require("json-server");
+var https = require("https");
+var fs = require("fs");
+var auth_1 = require("./auth");
+var authz_1 = require("./authz");
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+var HTTP_PORT = '3001';
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.post('/login', auth_1.handleAuthentication);
+server.use('/orders', authz_1.handleAuthorization);
+// Use default router
+server.use(router);
+var options = {
+    cert: fs.readFileSync('./backend/keys/cert.pem'),
+    key: fs.readFileSync('./backend/keys/key.pem')
+};
+https.createServer(options, server).listen(HTTP_PORT, function () {
+    console.log("JSON Server is running on https://localhost:" + HTTP_PORT);
+});
